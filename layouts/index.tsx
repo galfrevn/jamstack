02bcfc34ee35f4795a627b611d8502c7cca0e9d3
Franco Galfre <francogalfre2007@gmail.com>
2022-09-02 22:24:51 -0300

    [BUG] Styling bug fixed

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -9,11 +9,10 @@ const Layout: FC<{ children: ReactNode }> = ({ children }) => {
       <FractalNoise />
       <Header />
       <main
-        className="relative z-10 grid grid-cols-[1fr,min(640px,100%),1fr] gap-y-8 px-4
-       pt-48 text-lg text-teal-50/90 xl:grid-cols-[1fr,minmax(auto,280px),min(640px,100%),minmax(auto,280px),1fr] 
+        className="pt-48 relative z-10 grid grid-cols-[1fr,min(640px,100%),1fr] gap-y-8 px-4 text-lg text-[#F9F4DA] xl:grid-cols-[1fr,minmax(auto,280px),min(640px,100%),minmax(auto,280px),1fr] 
        xl:gap-x-8 xl:px-0 [&>*]:col-start-2 xl:[&>*]:col-start-3"
       >
-        {children}
+        <div className="-mt-12 sm:mt-0">{children}</div>
       </main>
       {/* <Footer /> */}
     </Fragment>
