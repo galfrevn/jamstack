df133ad45b7442b3ca113ff7becfe34b67a00d82
Valentin Galfre <galfre.vn@gmail.com>
2023-02-04 15:48:23 -0300

    Adding Metadata constant file

---
+++ constants/meta.ts
@@ -0,0 +1,7 @@
+export const URL = 'blog.galfrevn.com';
+export const TITLE = 'Galfrevn Hub';
+
+export const DESCRIPTION = `Find out all about Galfré Valentín, a fully completed Fullstack Developer from Argentina.
+Discover his blogs entries, where he uploads some daily problems solutions! 🚀`;
+
+export const CLOUDINARY = 'https://res.cloudinary.com/galfrevn/image/upload/v1676051850/blog/'
