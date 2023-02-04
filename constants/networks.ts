b59b1c7f2c6301c9b534c387829d08f9c41ef942
Valentin Galfre <galfre.vn@gmail.com>
2023-02-04 15:48:37 -0300

    Adding portfolio URL to network links

--- constants/networks.ts
+++ constants/networks.ts
@@ -3,6 +3,7 @@ import { TagProps } from 'components/Tag';
 export const linkedinUrl = 'https://linkedin.com/in/galfrevn';
 export const githubUrl = 'https://github.com/galfrevn';
 export const twitterUrl = 'https://twitter.com/galfrevn';
+export const portfolioUrl = 'https://galfrevn.com'
 
 export const socials: Omit<TagProps, 'icon'>[] = [
   {
