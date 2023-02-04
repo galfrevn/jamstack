cb3a1ad693930460de0f8653b26c874b9d6fdf34
Valentin Galfre <galfre.vn@gmail.com>
2023-02-04 17:12:47 -0300

    Moving old blogs page into index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,51 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType } from 'next';
+// Posts
+import { allPosts, type Post } from 'contentlayer/generated';
+
+// Social
+import { socials } from 'constants/networks';
+import { Link } from 'react-feather';
 
 // Components
-import Feed from "components/Feed";
-import Hero from "components/Hero";
-import Container from "components/Container";
+import Postcard from 'components/Postcard';
+import Footer from 'components/Footer';
+import Container from 'components/Container';
+import Hero from 'components/Hero';
+import TotalViews from 'components/TotalViews';
+import Update from 'components/Update';
+import Tag from 'components/Tag';
 
-const Home: NextPage = () => {
+export default function PostListPage({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Container>
-      <div className="-mt-12 sm:mt-0">
-        <Hero />
-        <Feed />
+      <Hero />
+
+      <div className='flex justify-start items-center gap-3'>
+        <TotalViews />
+
+        {socials.map((network, index) => (
+          <Tag {...network} key={network.label} index={index} icon={<Link size={13} />} />
+        ))}
       </div>
+
+      <Update />
+
+      <div className='-mt-4 pb-10'>
+        <div className='space-y-6 '>
+          <div className='flex space-x-2'></div>
+          {posts.map((post: Post, index: number) => (
+            <Postcard key={index} index={index} {...post} />
+          ))}
+        </div>
+      </div>
+      <Footer />
     </Container>
   );
-};
+}
 
-export default Home;
+// Get all posts
+export const getStaticProps: GetStaticProps<{ posts: Post[] }> = () => {
+  return { props: { posts: allPosts } };
+};
