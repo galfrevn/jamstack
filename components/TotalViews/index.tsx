47e94df242bed906c1da790ecf89cdd228b215f9
Valentin Galfre <galfre.vn@gmail.com>
2023-02-05 15:47:55 -0300

    Fixing total count component by removing loading animation

--- components/TotalViews/index.tsx
+++ components/TotalViews/index.tsx
@@ -18,26 +18,24 @@ const SomethingWentWrong = () => (
   </motion.div>
 );
 
-const CurrentCount = ({ count }: { count: number }) => (
-  <motion.div
-    key='total-count'
-    className='h-6 bg-neutral-500/20 border-neutral-500/40 border-2 rounded-md flex items-center justify-start pl-3 pr-4 select-none gap-2'
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
-    whileHover={{ scale: 1.05 }}
-  >
-    <Eye size={13} />
-    <p className='text-xs'>{count} views </p>
-  </motion.div>
-);
-
 const TotalViews = () => {
   const { data, error, isLoading } = useSWR<number>('api/count/total', fetcher);
 
-  if (isLoading) return <></>;
   if (error) return <SomethingWentWrong />;
 
-  return <CurrentCount count={data as number} />;
+  return (
+    <motion.div
+      key='total-count'
+      className='h-6 bg-neutral-500/20 border-neutral-500/40 border-2 rounded-md flex items-center justify-start pl-3 pr-4 select-none gap-2'
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+      whileHover={{ scale: 1.05 }}
+    >
+      <Eye size={13} />
+      {isLoading && <>...</>}
+      {data && <p className='text-xs'>{data} views </p>}
+    </motion.div>
+  );
 };
 
 export default TotalViews;
