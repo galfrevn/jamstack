3f21a27af3368b39707f9b4c6236179e3f025aa2
Valentin Galfre <galfre.vn@gmail.com>
2023-02-07 17:11:36 -0300

    Installing new fontsource fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,43 @@
-import "styles/globals.css";
-import type { AppProps } from "next/app";
-import { Toaster } from 'react-hot-toast'
+import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 
 // Fonts
-import "@fontsource/paytone-one";
-import "@fontsource/outfit"
+import '@fontsource/paytone-one';
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/500.css';
+import '@fontsource/outfit/600.css';
+import '@fontsource/outfit/700.css';
+import '@fontsource/outfit/900.css';
 
 // Routing & Layout
-import Router from "next/router";
-import Layout from "layouts";
+import Router from 'next/router';
+import Layout from 'layouts';
+
+import { SWRConfig } from 'swr';
+import { fetcher } from 'lib/swr';
 
 // NProgress bar
-import NProgress from "nprogress"; //nprogress module
-import "nprogress/nprogress.css"; //styles of nprogress
-import { AnimatePresence } from "framer-motion";
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+import { AnimatePresence } from 'framer-motion';
 
 //Binding events.
 NProgress.configure({
   showSpinner: false,
-  easing: "ease-out",
+  easing: 'ease-out',
   trickle: false,
-  speed: 300,
+  speed: 600,
 });
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AnimatePresence>
       <Layout>
-        <Toaster position="bottom-right" />
+        <Toaster position='bottom-right' />
         <Component {...pageProps} />
       </Layout>
     </AnimatePresence>
