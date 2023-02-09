b438e1c711e378b703bdc5a5765f479a3f1e29a0
Valentin Galfre <galfre.vn@gmail.com>
2023-02-09 17:13:50 -0300

    --date=6 days ago
    Creating API endpoint for getting a random word for the Wordle game

--- pages/api/wordle/dictionary.ts
+++ pages/api/wordle/dictionary.ts
@@ -1,11 +1,10 @@
-import type { NextApiResponse, NextApiRequest } from "next";
+import type { NextApiResponse, NextApiRequest } from 'next';
 import data from './data.json';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-
   /* Get a random word from the pool to be the answer */
   const { words } = data;
-  const answer = words[Math.floor(Math.random() * (words.length - 0 + 1) + 0)];
+  const answer = words[Math.floor(Math.random() * (words.length + 1))];
   const DATA = {
     dictionary: words,
     wordleAnswer: answer,
