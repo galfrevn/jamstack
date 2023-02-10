e181c9ffaf5fa35e858120c8d26f0d0d11863393
Valentin Galfre <galfre.vn@gmail.com>
2023-02-10 16:51:00 -0300

    Adding keyframe animations into tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      animation: {
-        enter: "enter 200ms ease-out",
-        "slide-in": "slide-in 1.2s cubic-bezier(.41,.73,.51,1.02)",
-        leave: "leave 150ms ease-in forwards",
-      },
-      keyframes: {
-        enter: {
-          "0%": { transform: "scale(0.9)", opacity: 0 },
-          "100%": { transform: "scale(1)", opacity: 1 },
+      keyframes: ({ theme }) => ({
+        mutation: {
+          '0%': {
+            background: theme('colors.neutral.600 / 0%'),
+            color: 'transparent'
+          },
+          '50%': {
+            background: theme('colors.neutral.600 / 10%'),
+          },
+          '100%': {
+            background: theme('colors.neutral.600 / 0%'),
+          },
         },
-        leave: {
-          "0%": { transform: "scale(1)", opacity: 1 },
-          "100%": { transform: "scale(0.9)", opacity: 0 },
+        loading: {
+          '0%': {
+            opacity: '.2',
+          },
+          '20%': {
+            opacity: '1',
+            transform: 'translateX(1px)',
+          },
+          to: {
+            opacity: '.2',
+          },
         },
-        "slide-in": {
-          "0%": { transform: "translateY(-100%)" },
-          "100%": { transform: "translateY(0)" },
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
         },
-      },
+      }),
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [require('@tailwindcss/line-clamp')],
 };
