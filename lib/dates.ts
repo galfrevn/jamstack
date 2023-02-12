06fc8720627771914475977e95ae928d8ba568f4
Valentin G <galfre.vn@gmail.com>
2023-02-12 20:27:18 -0300

    Fixing dates operations with valueof()

--- lib/dates.ts
+++ lib/dates.ts
@@ -1,6 +1,6 @@
 export const hasOneDayPassed = (targetDate: Date) => {
   const currentDate = new Date();
-  const difference = currentDate - targetDate;
+  const difference = currentDate.valueOf() - targetDate.valueOf();
   const oneDay = 24 * 60 * 60 * 1000;
 
   return difference >= oneDay;
